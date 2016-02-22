515ae6137d5a21739b7557882d6b5f380784c169
An Ta <atta2@asu.edu>
2016-02-22 14:48:01 -0700

    Fixed misspled return

--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -20,7 +20,7 @@ public class Savings extends Account {
 			balance = balance + amount - 0.50F;
 			if (balance >= 0.0f) {
 				setState(State.OPEN);
-				retrun true;
+				return true;
 			}
 		}
 		return false;
